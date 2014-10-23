f27b921d3a382893ec5e1edf2a401f9db45fb805
lewcok <lewco@foxmail.com>
2014-10-23 10:01:52 +0800

    Initial Commit

---
+++ SocketTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SocketTest
+//
+//  Created by laukevin on 14-10-23.
+//  Copyright (c) 2014年 laukevin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
